399badf882f95fbda387edce3169d9cb94a5ecf0
juancastillomosquera <131283076+juancastillomosquera@users.noreply.github.com>
2026-01-25 19:34:27 -0500

    Add environments

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export interface Product {
     id: string;
@@ -26,7 +27,7 @@ export interface ProductResponse {
 })
 export class ProductService {
     private http = inject(HttpClient);
-    private apiUrl = 'http://localhost:3000/products';
+    private apiUrl = `${environment.backend_url}products`;
 
     getProducts(page: number, limit: number, query: string = ''): Observable<ProductResponse> {
         let params = new HttpParams()
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+    production: true,
+    backend_url: 'http://localhost:3000'
+};
